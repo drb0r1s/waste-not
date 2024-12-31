88c9c4822f7183fd3a9da447a7a1b8ccc0eed3f4
Boris <drb0r1sdev@gmail.com>
2024-12-31 18:19:19 +0100

    Package and router updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import "./style/main.css";
 import { routes } from './data/routes';
 import { Storage } from './functions/Storage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 Storage.initialization();
 
