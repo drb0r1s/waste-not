410acbb4daac7c461f64f31b2dbb31f088db8aa8
Boris <drb0r1sdev@gmail.com>
2025-01-23 17:25:47 +0100

    Gun updates

--- src/functions/Storage.js
+++ src/functions/Storage.js
@@ -22,7 +22,7 @@ export const Storage = {
     },
 
     synchronizeVersion: () => {
-        const VERSION = "V1";
+        const VERSION = "V2";
 
         if(localStorage.getItem("WASTENOT_VERSION")) {
             const item = localStorage.getItem("WASTENOT_VERSION");
